8e6f8559298ba157e8853ea62d3db1953bb16a74
Desmond Okeke <okekedesmond71@gmail.com>
2025-02-24 08:39:09 +0100

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3100);
 
-  console.log(`http://localhost:${process.env.PORT ?? 3000}`);
+  console.log(`http://localhost:${process.env.PORT ?? 3100}`);
 }
 bootstrap();
